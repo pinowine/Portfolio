8434d17413df5a7cae68d8f31bea9800088391ab
pinowine <692975193@qq.com>
2024-06-14 22:25:19 +0800

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import replace from '@rollup/plugin-replace';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    replace({
+      'F:/Portfolio/Portfolio/src/assets/': 'https://ibuprofennist.com/portfolio/',
+      preventAssignment: true
+    })
+  ],
+})
